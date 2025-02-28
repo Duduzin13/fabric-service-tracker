f3e0975826bc70c666e467286cb074f191208ef5
Duduzin <90728891+Duduzin13@users.noreply.github.com>
2025-02-28 17:55:41 -0300

    Delete src directory

--- src/pages/Services.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-import ServiceList from '@/components/ServiceList';
-
-export default function Services() {
-  const { clientId } = useParams();
-  return <ServiceList clientId={clientId || ''} />;
-} 
