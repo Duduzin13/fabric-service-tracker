2dc08c03fb88178ff2a56f0a966a68f31de1fd53
duduzin <eduardoogabrielloliveiraa@gmail.com>
2025-02-28 18:15:49 -0300

    Atualiza o código com a versão da pasta Cadastro

--- Desktop/Nova pasta (3)/Cadastro/clean.ps1
+++
@@ -1,7 +0,0 @@
-Get-ChildItem -Path "src" -Recurse -File | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw
-    $content = $content -replace '<<<<<<< HEAD', ''
-    $content = $content -replace '=======', ''
-    $content = $content -replace '>>>>>>> \w+', ''
-    Set-Content $_.FullName $content
-} 
